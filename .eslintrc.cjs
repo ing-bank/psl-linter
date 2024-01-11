311775f65e8ad5f877b288a198baa4237e776ad4
Mischa Reitsma <reitsma.mischa@gmail.com>
2024-01-11 07:04:27 +0100

    Change tslint to eslint
    Change the use of tslin to the use of eslint with TypeScript plugins.
    Again, most of it has been copied from the psl-parser project. First the
    source will be cleaned from errors, and unit test should still pass.
    After that, the unit tests will be updated.
    
    This commit also removes the pretest npm script. The use of ts-jest
    module allows us to run without compiling any TypeScript file at all,
    and compiling also gave issue where just one test (config.test.ts) ran
    twice, once the TypeScript version, and once the out/config.test.js
    file.

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+/* eslint-env node */
+module.exports = {
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended-type-checked"
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"project": true,
+		"tsconfigRootDir": __dirname,
+		"warnOnUnsupportedTypeScriptVersion": true,
+	},
+	"ignorePatterns": [
+		"jest.config.js",
+		".eslintrc.cjs",
+		"**/out/**",
+		"**/dist/**",
+		"**/lib/**",
+		"**/node_modules/**",
+		"build-package-json.js"
+	],
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"env": {
+		"node": true,
+	},
+	"rules": {
+		"quotes": "off",
+		"@typescript-eslint/quotes": 
+			[
+				"error",
+				"single",
+				{
+					"avoidEscape": true
+				}
+			],
+		"semi": "off",
+		"@typescript-eslint/semi": ["error"],
+		"max-len": [
+			"error",
+			{
+				"code": 100,
+				"tabWidth": 8,
+				"ignorePattern": "^\\s*// (TODO|FIXME|eslint-).*$",
+			},
+		],
+	},
+	"root": true,
+};
